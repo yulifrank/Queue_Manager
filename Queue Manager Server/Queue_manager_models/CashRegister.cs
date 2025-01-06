d05d6a8fde57d1306a407e94be521a2331be45b4
yulifrank <148456398+yulifrank@users.noreply.github.com>
2025-01-06 12:49:43 +0200

    change class

--- Queue Manager Server/Queue_manager_models/CashRegister.cs
+++ Queue Manager Server/Queue_manager_models/CashRegister.cs
@@ -6,12 +6,31 @@ using System.Threading.Tasks;
 
 namespace Queue_manager_models
 {
-    internal class Position
+    public class CashRegister
     {
-        public int Id { get; set; }
-        public string representative { get; set; }
-        public Person Person { get; set; }
-        public Queue Queue { get; set; }
-        
+        public int RegisterId { get; set; }
+        public Person CurrentCustomer { get; set; }
+
+        public CashRegister(int registerId)
+        {
+            RegisterId = registerId;
+            CurrentCustomer = null;
+        }
+
+        // פונקציה שמטפלת בלקוח בקופה צריך לבדוק איך לממש את זה טוב!!
+        public void ProcessCustomer()
+        {
+            if (CurrentCustomer != null)
+            {
+                Console.WriteLine($"Processing customer {CurrentCustomer.ToString()} at register {RegisterId}");
+                CurrentCustomer = null;
+            }
+        }
+
+        public bool IsAvailable()
+        {
+            return CurrentCustomer == null;
+        }
     }
+
 }
