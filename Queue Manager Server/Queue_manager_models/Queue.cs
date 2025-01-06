1458df82bda6e6ea0e3fa6ebbf27f7a415daa59d
yulifrank <148456398+yulifrank@users.noreply.github.com>
2025-01-06 12:45:14 +0200

    change in models

--- Queue Manager Server/Queue_manager_models/Queue.cs
+++ Queue Manager Server/Queue_manager_models/Queue.cs
@@ -6,9 +6,38 @@ using System.Threading.Tasks;
 
 namespace Queue_manager_models
 {
-    internal class Queue
+    public class Queue
     {
-        public Person Person { get; set; }
+        public int QueueId { get; set; }
+        public List<Person> People { get; set; }
 
+        public Queue(int queueId)
+        {
+            QueueId = queueId;
+            People = new List<Person>();
+        }
+
+        public void AddPerson(Person person)
+        {
+            People.Add(person);
+        }
+
+        public Person RemovePerson()
+        {
+            if (People.Count > 0)
+            {
+                var person = People[0];
+                People.RemoveAt(0);
+                return person;
+            }
+            return null;
+        }
+
+        // פונקציה שמחזירה את מספר האנשים בתור
+        public int GetQueueLength()
+        {
+            return People.Count;
+        }
     }
+
 }
